73ffcd3ff0805441f82dc0d6fcf56a931b35413f
Madddy4 <madeshw1994@gmail.com>
2025-11-28 11:48:38 +0530

    added uat

--- cypress/e2e/a11y-basic.cy.js
+++ cypress/e2e/a11y-basic.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Basic Accessibility Test', () => {
+
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:3000/')
+    cy.injectAxe()
+  })
+
+  it('Should detect accessibility violations', () => {
+    cy.checkA11y()
   })
 })
